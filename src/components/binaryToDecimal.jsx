847b2de5dde835f160579799b7ba0035c20e8c0d
tzchrzanowski <tzchrzanowski@gmail.com>
2019-04-14 20:36:59 +0200

    added user input field for binary to decimal calculation

--- src/components/binaryToDecimal.jsx
+++ src/components/binaryToDecimal.jsx
@@ -7,17 +7,22 @@ export default class BinaryToDecimal extends Component {
     this.state = {
       N: 0,
       result: 0,
-      showResult: 'display:none'
+      showResult: 'display:none',
+      input: 0,
+      disabledButton: true,
     }
     this.translateToDecimal = this.translateToDecimal.bind(this);
+    this.getInputValue = this.getInputValue.bind(this);
   }
 
-  translateToDecimal(props) {
-    let n = props.input.length;
+  translateToDecimal() {
+    console.log('asd')
+    console.log(this.state.input)
+    let n = this.state.input.length;
     let currentN = n;
     let sum = 0;
-    for (let i = 0; i<props.input.length; i++){
-      if(props.input[i] == 1) {
+    for (let i = 0; i< this.state.input.length; i++){
+      if(this.state.input[i] == 1) {
         sum = sum += Math.pow(2, currentN-1); 
       }
       currentN -= 1;
@@ -28,11 +33,37 @@ export default class BinaryToDecimal extends Component {
     });
   }
 
+  getInputValue(value) {
+    console.log(value);
+    if (/^[01.]{0,30}$/.test(value)) {
+      this.setState({
+        input: value,
+        disabledButton: false,
+      })
+
+    } else {
+      this.setState({
+        input: "Only these characters are allowed: '0', '1', '.'",
+        disabledButton: true,
+      })
+    }
+  }
+
   render() {
     return <div id="reactBodyComp">
 		  <p>Binary to decimal</p>
-      <button onClick={()=> this.translateToDecimal(this.props)}>calculate</button>
-      <p>Number in binary : {this.props.input}</p>
+      <input 
+        type='number'
+        pattern="[01.]{0,30}"
+        value={this.state.input}
+        onChange={(e) => this.getInputValue(e.target.value)}>
+      </input>
+      {this.state.disabledButton ? 
+        <button onClick={()=> this.translateToDecimal()} disabled>calculate</button>
+        :
+        <button onClick={()=> this.translateToDecimal()}>calculate</button>
+      }
+      <p>Number in binary : {this.state.input}</p>
       <p style={this.state.showResult}>Number in decimal : {this.state.result}</p>
 	  </div>;
   }
